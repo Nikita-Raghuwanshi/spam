b0c1ae2a4b945d65dcced1414cb772c993661346
Nikita_Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2025-05-08 00:20:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
